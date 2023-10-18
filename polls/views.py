bb0e85e1f892c7918b2a31fcce7efa634ee9eb4d
Yami Naik <yaminaik@yamis-air.mynetworksettings.com>
2023-10-18 15:38:34 -0400

    solved flake error

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,6 @@ from django.http import HttpResponseRedirect
 from django.views import generic
 
 
-
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
